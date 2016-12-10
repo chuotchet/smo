04508e5cfa23d980b04e00eefdfb5c240f56ba41
chuotchet <chuotchet94@gmail.com>
2016-12-10 13:38:15 +0700

    dd

--- testdb.js
+++ testdb.js
@@ -1,5 +1,16 @@
 var models = require("./models");
 
-models.Gateway.getGatewayByMAC('qwe', function(gw){
-  console.log(gw.dataValues);
-});
+// models.Gateway.getGatewayByMAC('qwe', function(gw){
+//   console.log(gw.dataValues);
+// });
+var data = {
+  N_MAC: 'dfd',
+  key: 'dd',
+  name: 'dd'
+}
+//models.sequelize.sync();
+//models.Gateway.sync();
+//models.Node.create(data);
+// models.Node.getNodeByMAC('0013a20040ac1aed', function(node){
+//   console.log(node.dataValues);
+// });
